654918d8432123f9c7f14df23e36cf5b04d3eae5
Justin Ong <justin.ong.97@gmail.com>
2017-01-27 07:15:46 +0800

    added handling for various messages

--- Grbl.js
+++ Grbl.js
@@ -17,7 +17,11 @@ let Grbl = function(port_name) {
   });
   
   this.serial_port.on("open", function(data) {
-    self.logger.info("Open: {0}".format(data));
+    self.logger.info("Port open");
+  });
+  
+  this.serial_port.on("close", function(data) {
+    self.logger.info("Port closed");
   });
   
   this.serial_port.on("data", function(line) {
@@ -25,7 +29,35 @@ let Grbl = function(port_name) {
     if (line.length === 0) return;
     
     self.logger.info("Data: {0}".format(line));
+    
+    // handles welcome messages
+    if (line.startsWith("Grbl")) {
+      self.logger.info("GRBL initialised!");
+    }
+    // handles alarm messages
+    else if (line.startsWith("ALARM:")) {
+      
+    }
+    // handles settings messages
+    else if (line.startsWith("$")) {
+      
+    }
+    // handles feedback messages
+    else if (/^\[(.+)\]/.test(line) {
+      
+    }
+    // handles real-time status reports
+    else if (/^<(.+)>/.test(line)) {
+      let data_fields = line.substring(1, line.length-1).split("|");
+      console.log(data_fields);
+    }
   });
+  
+  setInterval(function() {
+    if (!self.serial_port.isOpen()) return;
+    
+    self.serial_port.write("?");
+  }, config.status_report_poll_interval);
 }
 
 module.exports = Grbl;
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // file containing http://www.linux-usb.org/usb.ids
   usb_id_list: "usb.ids.txt",
-  uart_rate: 115200
+  uart_rate: 115200,
+  // in ms, documentation says no more than 5Hz
+  status_report_poll_interval: 200
 }
