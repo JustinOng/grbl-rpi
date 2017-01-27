012408afc444be8efc7b11554d61d95071b7da1b
Justin Ong <justin.ong.97@gmail.com>
2017-01-27 14:38:54 +0800

    separated handling of serial input to different function

--- Grbl.js
+++ Grbl.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let config = require("./config.js");
-let SerialPort = require("serialport");
 let logger = require("./log.js");
 let CSVCodes = require("./CSVCodes.js");
+
+let SerialPort = require("serialport");
+let util = require("util");
 
 let EventEmitter = require('events').EventEmitter;
 
@@ -27,45 +29,7 @@ let Grbl = function(port_name) {
     self.logger.info("Port closed");
   });
   
-  this.serial_port.on("data", function(line) {
-    // something funky with the splitting of lines
-    if (line.length === 0) return;
-    
-    self.logger.info("Data: {0}".format(line));
-    
-    // handles welcome messages
-    if (line.startsWith("Grbl")) {
-      self.logger.info("GRBL initialised!");
-      
-      self.emit("initialised");
-    }
-    // handles alarm messages
-    else if (line.startsWith("ALARM:")) {
-      let alarm_code = line.substring(6, line.length);
-      
-      if (!alarm_code in CSVCodes.alarm_codes) {
-        logger.warn("Unknown error code: {0}".format(alarm_code));
-        return;
-      }
-      
-      logger.error("ALARM: {0}".format(CSVCodes.alarm_codes[alarm_code].message));
-      
-      self.emit("alarm", CSVCodes.alarm_codes[alarm_code]);
-    }
-    // handles settings messages
-    else if (line.startsWith("$")) {
-      
-    }
-    // handles feedback messages
-    else if (/^\[(.+)\]/.test(line)) {
-      
-    }
-    // handles real-time status reports
-    else if (/^<(.+)>/.test(line)) {
-      let data_fields = line.substring(1, line.length-1).split("|");
-      console.log(data_fields);
-    }
-  });
+  this.serial_port.on("data", self.process_line.bind(self));
   
   setInterval(function() {
     if (!self.serial_port.isOpen()) return;
@@ -74,6 +38,48 @@ let Grbl = function(port_name) {
   }, config.status_report_poll_interval);
 }
 
-util.inherits(SerialManager, EventEmitter);
+Grbl.prototype.process_line = function(line) {
+  let self = this;
+  
+  // something funky with the splitting of lines
+  if (line.length === 0) return;
+  
+  self.logger.info("Data: {0}".format(line));
+  
+  // handles welcome messages
+  if (line.startsWith("Grbl")) {
+    self.logger.info("GRBL initialised!");
+    
+    self.emit("initialised");
+  }
+  // handles alarm messages
+  else if (line.startsWith("ALARM:")) {
+    let alarm_code = line.substring(6, line.length);
+    
+    if (!alarm_code in CSVCodes.alarm_codes) {
+      logger.warn("Unknown error code: {0}".format(alarm_code));
+      return;
+    }
+    
+    logger.error("ALARM: {0}".format(CSVCodes.alarm_codes[alarm_code].message));
+    
+    self.emit("alarm", CSVCodes.alarm_codes[alarm_code]);
+  }
+  // handles settings messages
+  else if (line.startsWith("$")) {
+    
+  }
+  // handles feedback messages
+  else if (/^\[(.+)\]/.test(line)) {
+    
+  }
+  // handles real-time status reports
+  else if (/^<(.+)>/.test(line)) {
+    let data_fields = line.substring(1, line.length-1).split("|");
+    console.log(data_fields);
+  }
+}
+
+util.inherits(Grbl, EventEmitter);
 
 module.exports = Grbl;
